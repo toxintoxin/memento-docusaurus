041e6d8c013a55f3db58ab3ee7612c2fc3b8d1bf
toxintoxin <839868794@qq.com>
2024-03-28 15:58:19 +0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
     <Layout
       title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-        <header className={clsx(styles.heroBanner)}>
+      <header className={clsx(styles.heroBanner)} style={{height: "calc(100vh - 60px)"}}>
         <div className="container">
-          <img src={socialCard} style={{width: 400}}/>
+          <img src={socialCard} style={{width: "calc(100vh)"}}/>
           <h1 className="hero__title">Hola, here is <span className={styles.highlighted}>{siteConfig.title}</span></h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
         </div>
