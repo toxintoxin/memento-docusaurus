88f61eef9b2c66225f879f96dceaa3717dc84119
toxintoxin <839868794@qq.com>
2024-03-31 00:00:02 +0800

    blog update

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import styles from './index.module.css';
 
-import socialCard from "@site/static/img/social-card.jpg";
+import homeImg from "@site/static/img/home-img.jpg";
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
@@ -13,7 +13,7 @@ export default function Home() {
       description="Description will go into a meta tag in <head />">
       <header className={clsx(styles.heroBanner)} style={{height: "calc(100vh - 60px)"}}>
         <div className="container">
-          <img src={socialCard} style={{width: "calc(100vh)"}}/>
+          <img src={homeImg} style={{width: "calc(100vh)"}}/>
           <h1 className="hero__title">Hola, here is <span className={styles.highlighted}>{siteConfig.title}</span></h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
         </div>
