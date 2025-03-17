8b51eaada16fc80b1a141d5e7345f645d5663e5b
toxintoxin <839868794@qq.com>
2025-03-17 13:59:30 +0800

    add search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -157,6 +157,24 @@ const config = {
         darkTheme: prismThemes.dracula,
       },
     }),
+    themes: [
+      // ... Your other themes.
+      [
+        require.resolve("@easyops-cn/docusaurus-search-local"),
+        /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+        ({
+          // ... Your options.
+          // `hashed` is recommended as long-term-cache of index file is possible.
+          hashed: true,
+  
+          // For Docs using Chinese, it is recomended to set:
+          // language: ["en", "zh"],
+  
+          // If you're using `noIndex: true`, set `forceIgnoreNoIndex` to enable local index:
+          // forceIgnoreNoIndex: true,
+        }),
+      ],
+    ],
 
 };
 
